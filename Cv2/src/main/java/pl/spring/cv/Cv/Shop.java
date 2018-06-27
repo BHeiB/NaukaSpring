494d6189b9881762975376abc262ff1250e4b353
BHeiB <radeknasalski5@gmail.com>
2018-06-27 19:55:32 +0200

    end part 6

--- Cv2/src/main/java/pl/spring/cv/Cv/Shop.java
+++ Cv2/src/main/java/pl/spring/cv/Cv/Shop.java
@@ -3,23 +3,49 @@ package pl.spring.cv.Cv;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/api")
 public class Shop {
 	
 	
-	private ShoppingCard shoppingCard;
-	
-	@Autowired
-	public void setShoppingCard(ShoppingCard shoppingCard) {
-		this.shoppingCard = shoppingCard;
+	@RequestMapping(value = "/example", method = RequestMethod.GET)
+	@ResponseBody
+	public String purchase() {
+		return "example";
 	}
 	
-	public void purchase() {
-		shoppingCard.addProductToShoppingCard(new Product(1, "Milk"));
-		shoppingCard.addProductToShoppingCard(new Product(2, "Bread"));
+	@RequestMapping("/exampleRequestHeader")
+	@ResponseBody
+	public String requestHeaderExample(@RequestHeader("number") Long number) {
+		return "dostarczone przez naglowek " + number;
 	}
+	@RequestMapping("/requestParam")
+	@ResponseBody
+	public String requestParamExample(@RequestParam("number") Long number) {
+		return "dostarczone przez naglowek " + number;
+	}
+	@RequestMapping("/examplePathVariable/{number}")
+	@ResponseBody
+	public String pathVariableExample(@PathVariable("number") Long number) {
+	    return "delivered by PathVariable: " + number;
+	}
+	
+	@RequestMapping(value = "/requestBody", method = RequestMethod.POST)
+	@ResponseBody
+	public String requestBodyExample(@RequestBody Long number) {
+	    return "delivered by RequestBody: " + number;
+	}
+	
+
 
 
 }
