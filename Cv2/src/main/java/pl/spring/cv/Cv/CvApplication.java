a463e603a7e5228cdaad93fdf0d811a4100df1e0
BHeiB <radeknasalski5@gmail.com>
2018-06-27 15:40:21 +0200

    part 2

--- Cv2/src/main/java/pl/spring/cv/Cv/CvApplication.java
+++ Cv2/src/main/java/pl/spring/cv/Cv/CvApplication.java
@@ -1,14 +1,25 @@
 package pl.spring.cv.Cv;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = Hello.class)
+//@ComponentScan(basePackageClasses = Hello.class)
 public class CvApplication {
+	
+	@Autowired
+	Hello2 hello;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CvApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println(hello.sayHello());
 	}
 }
